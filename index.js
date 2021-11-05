c73e7f25fac2ce692bc7f6cda24b0d511bbde31e
Tom Cummings <cummings.t287@gmail.com>
2021-11-05 12:58:35 -0400

    Add json body parser

--- index.js
+++ index.js
@@ -2,25 +2,28 @@ require("dotenv").config();
 
 const mongoUtil = require("./mongoUtil");
 
-mongoUtil.connectToServer(function(err, mongoClient) {
+const routeBuilder = require("./routes/routes");
+
+mongoUtil.connectToServer(function(err) {
     if (err) {
         console.error(err);
         return;
     }
 
     const express = require("express");
+    const parser = require("body-parser");
+
     const app = express();
+    const router = express.Router();
     const port = 3000;
+
+    routeBuilder.buildRoutes(router, mongoUtil.getDb());
+
+    app.use(parser.json());
+
+    app.use("/", router);
 
     app.listen(port, () => {
         console.log(`Server running on port ${port}`);
-    });
-
-    app.use(express.urlencoded({ extended: false }));
-
-    app.use((req, res, next) => {
-        console.log(req.params);
-        console.log(req.body);
-        next();
     });
 });
