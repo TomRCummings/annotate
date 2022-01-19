bfb144f29e73da7596c099a3f594d520e2c813cd
Tom Cummings <cummings.t287@gmail.com>
2022-01-19 11:35:25 -0500

    Add auth to protected routes

--- routes/routes.js
+++ routes/routes.js
@@ -1,13 +1,36 @@
+const mongo = require("mongodb");
+
 const userRoutes = require("./user-routes");
 const collectionRoutes = require("./collection-routes");
 const objectRoutes = require("./object-routes");
-// const labelRoutes = require("./label-routes");
+const labelRoutes = require("./label-routes");
 
-function buildRoutes(router, db) {
-    userRoutes.addUserRoutes(router, db);
+function buildRoutes(router, db, authClient) {
+    router.use(async function(req, res, next) {
+        if (req.path == "/" || req.path == "/sign-in" || req.path == "/sign-in/" || req.path == "/auth" || await isUserAuthenticated(req, db)) {
+            next();
+        } else {
+            res.status(401);
+            res.send("You are not logged in! Visit the main page to log in.");
+            next("router");
+        }
+    });
+    userRoutes.addUserRoutes(router, db, authClient);
     collectionRoutes.addCollectionRoutes(router, db);
     objectRoutes.addObjectRoutes(router, db);
-    // labelRoutes.addLabelRoutes(router, db);
+    labelRoutes.addLabelRoutes(router, db);
+}
+
+async function isUserAuthenticated(req, db) {
+    const sessTable = db.collection("sessions");
+    const sessID = await req.headers.authorization;
+    if (sessID !== undefined) {
+        const result = await sessTable.find({ _id: new mongo.ObjectId(sessID) });
+        const count = await result.count();
+        return (count > 0);
+    } else {
+        return false;
+    }
 }
 
 module.exports = { buildRoutes };
